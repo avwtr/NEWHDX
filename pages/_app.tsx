f8dfcedf4dd5d040f56da723d57ad5c5418c32b8
avwtr <alexvawter.ime@gmail.com>
2025-06-12 11:31:28 -0400

    analytics wasnt added properly. now should work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,30 @@ if (process.env.NODE_ENV === 'production') {
 
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      {/* Plausible Analytics */}
+      <Script
+        strategy="afterInteractive"
+        data-domain="heterodoxlabs.com"
+        src="https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js"
+      />
+      <Script
+        id="plausible-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.plausible = window.plausible || function() {
+              (window.plausible.q = window.plausible.q || []).push(arguments)
+            }
+          `,
+        }}
+      />
     </>
   )
 }
@@ -24,4 +42,4 @@ if (process.env.NODE_ENV === 'development') {
     if (args[0]?.includes?.('next-dev-overlay')) return
     originalError.call(console, ...args)
   }
-} 
+}
